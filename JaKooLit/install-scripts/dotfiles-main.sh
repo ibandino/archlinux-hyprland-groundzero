7ba3654c81cd22ed62aabb8f75d16c910f84d514
iBandino <55088442+ibandino@users.noreply.github.com>
2025-04-18 13:39:49 +0200

    Update dotfiles-main.sh
    Signed-off-by: iBandino <55088442+ibandino@users.noreply.github.com>

--- JaKooLit/install-scripts/dotfiles-main.sh
+++ JaKooLit/install-scripts/dotfiles-main.sh
@@ -2,35 +2,35 @@
 # 💫 https://github.com/JaKooLit 💫 #
 # Hyprland-Dots to download from main #
 
+set -e
 
-## WARNING: DO NOT EDIT BEYOND THIS LINE IF YOU DON'T KNOW WHAT YOU ARE DOING! ##
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Change the working directory to the parent directory of the script
-PARENT_DIR="$SCRIPT_DIR/.."
-cd "$PARENT_DIR" || { echo "${ERROR} Failed to change directory to $PARENT_DIR"; exit 1; }
-
-# Source the global functions script
+# Farbdefinitionen laden
 if ! source "$(dirname "$(readlink -f "$0")")/Global_functions.sh"; then
-  echo "Failed to source Global_functions.sh"
+  echo -e "[ERROR] Global_functions.sh konnte nicht geladen werden!"
   exit 1
 fi
 
-# Check if Hyprland-Dots exists
-printf "${NOTE} Cloning and Installing ${SKY_BLUE}KooL's Hyprland Dots${RESET}....\n"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PARENT_DIR="$SCRIPT_DIR/.."
+cd "$PARENT_DIR" || { echo -e "${ERROR} Konnte nicht ins Verzeichnis $PARENT_DIR wechseln!"; exit 1; }
 
+echo -e "${NOTE} Cloning and Installing ${SKY_BLUE}GroundZero Hyprland Dots${RESET}..."
+
+# Ordner vorhanden?
 if [ -d Hyprland-Dots ]; then
-  cd Hyprland-Dots
+  cd Hyprland-Dots || exit 1
   git stash && git pull
   chmod +x copy.sh
-  ./copy.sh 
+  ./copy.sh
 else
+  # Frisch klonen
   if git clone --depth=1 https://github.com/JaKooLit/Hyprland-Dots; then
     cd Hyprland-Dots || exit 1
     chmod +x copy.sh
-    ./copy.sh 
+    ./copy.sh
   else
-    echo -e "$ERROR Can't download ${YELLOW}KooL's Hyprland-Dots${RESET} . Check your internet connection"
+    echo -e "${ERROR} Download von ${YELLOW}KooL's Hyprland-Dots${RESET} fehlgeschlagen – Check deine Internetverbindung."
+    exit 1
   fi
 fi
 
