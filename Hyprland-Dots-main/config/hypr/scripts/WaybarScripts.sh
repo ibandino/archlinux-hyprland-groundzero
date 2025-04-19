1d758cb2c9b5ad669be5e979455d8b1cf225d387
iBandino <55088442+ibandino@users.noreply.github.com>
2025-04-19 14:26:17 +0200

    Update WaybarScripts.sh
    Info : ich geh davon aus das es hierbei um das standart layout von waybar geht ....
    
    da wir hier das hyprland von JaKooLit fahren , werden wir zu ehren beim standart bleiben (nur ohne update funktion)
    
    
    # Define the path to the config file
    config_file=$HOME/archlinux-hyprland-groundzero/Hyprland-Dots-main/.config/hypr/UserConfigs/01-UserDefaults.conf
    
    
    Signed-off-by: iBandino <55088442+ibandino@users.noreply.github.com>

--- Hyprland-Dots-main/config/hypr/scripts/WaybarScripts.sh
+++ Hyprland-Dots-main/config/hypr/scripts/WaybarScripts.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  #
+
 # This file used on waybar modules sourcing defaults set in $HOME/.config/hypr/UserConfigs/01-UserDefaults.conf
 
 # Define the path to the config file
-config_file=$HOME/.config/hypr/UserConfigs/01-UserDefaults.conf
+config_file=$HOME/archlinux-hyprland-groundzero/Hyprland-Dots-main/.config/hypr/UserConfigs/01-UserDefaults.conf
 
 # Check if the config file exists
 if [[ ! -f "$config_file" ]]; then
