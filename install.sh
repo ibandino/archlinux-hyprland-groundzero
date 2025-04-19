37e3ef00e46c755ccfd33e719ce6f8061413fb68
iBandino <55088442+ibandino@users.noreply.github.com>
2025-04-19 03:37:46 +0200

    Update install.sh
    angepaste install.sh um visuell im install zu zeigen welche punkte gerade gestartet werden 
    
    Signed-off-by: iBandino <55088442+ibandino@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -4,21 +4,37 @@
 echo -e "\e[1;32m[+] Starte Groundzero Setup...\e[0m"
 sleep 1
 
+echo -e "\e[1;32m[+] initialisiere \e[0m"
+sleep 1
+
+echo -e "\e[1;32m[+] # 0.GroundZero - Gemeinsame folge packete \e[0m"
+sleep 1
 # 0.GroundZero - Gemeinsame folge packete
 bash ./script/corepackages.sh
 
+echo -e "\e[1;32m[+] # 1. Tools - grundaustattung \e[0m"
+sleep 1
 # 1. Tools
 bash ./script/groundzerotools.sh
 
+
+echo -e "\e[1;32m[+] # 2. Gamescript \e[0m"
+sleep 1
 # 2. Gamescript
 bash ./script/gamescript.sh
 
+echo -e "\e[1;32m[+] # 3. Hyprland JaKooLit Style + Updates + FullConfigs \e[0m"
+sleep 1
 # 3. Hyprland JaKooLit + Updates
 bash ./script/hyprland.sh
 
-# 4. Labfactory (noch leer)
+echo -e "\e[1;32m[+] # 4. Labfactory Sektion\e[0m"
+sleep 1
+# 4. Labfactory Sektion
 bash ./script/labfactory.sh
 
+echo -e "\e[1;32m[+] # 5. CleanUp Sektion \e[0m"
+sleep 1
 # 5. CleanUp Sektion 
 bash ./script/clean_groundzero.sh
 
